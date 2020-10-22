80574f3fc1846424062a5ade873d01e9eb4c897c
DAVIDE <davideamabile81gmail.com>
2020-10-22 15:31:28 +0200

    inizio

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+// creare una classe user
+class User{
+// creare attributi nella classe
+public $surname;
+public $nation;
+public $number;
+public $role;
+}
+
+
+// creazione oggetti
+$player1 = new User();
+$player1->surname ="Julio Cesar"
+$player1->nation = "Brasile"
+$player1->number = 12
+$player1->role ="Portiere";
+
+var_dump($player1);
+
+
+
