fe8c0da0a2bfbd58f043d45cfb27e6714351e314
DAVIDE <davideamabile81gmail.com>
2020-10-22 17:10:14 +0200

    funzione

--- index.php
+++ index.php
@@ -8,14 +8,17 @@ public $number;
 public $role;
 }
 
+// creare funzione constructor
+function __constructor($_surname, $_nation, $_number, $_role)
+{
+    $this->surname=$_surname;
+    $this->nation=$_nation;
+    $this->number=$_number;
+    $this->role=$_role;
+}
 
-// creazione oggetti
-$player1 = new User();
-$player1->surname ="Julio Cesar"
-$player1->nation = "Brasile"
-$player1->number = 12
-$player1->role ="Portiere";
-
+// creiamo gli oggetti
+$player1= new User("Julio Cesar", "Brasile", "12", "Portiere");
 var_dump($player1);
 
 
