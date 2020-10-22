9b0f02a1e1f9964a020880acea7983e7fa66fe87
DAVIDE <davideamabile81gmail.com>
2020-10-22 17:47:26 +0200

    array oggetti

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ public $role;
 
 
 // creare funzione constructor
-function __constructor($_surname, $_nation, $_number, $_role)
+function __construct($_surname, $_nation, $_number, $_role)
 {
     $this->surname=$_surname;
     $this->nation=$_nation;
@@ -21,7 +21,41 @@ function __constructor($_surname, $_nation, $_number, $_role)
 
 // creiamo gli oggetti
 $player1 = new User("Julio Cesar", "Brasile", "12", "Portiere");
-var_dump($player1);
+$player2 = new User("Maicon", "Brasile", "13", "Difensore");
+$player3 = new User("Lucio", "Brasile", "6", "Difensore");
+$player4 = new User("Samuel", "Argentina", "25", "Difensore");
+$player5 = new User("Chivu", "Romania", "26", "Difensore");
+$player6 = new User("Zanetti", "Argentina", "4", "Centrocampista");
+$player7 = new User("Cambiasso", "Argentina", "19", "Centrocampista");
+$player8 = new User("Pandev", "Macedonia", "27", "Ala");
+$player9 = new User("Eto'o", "Camerun", "4", "Ala");
+$player10 = new User("Sneijder", "Olanda", "10", "Trequartista");
+$player11 = new User("Milito", "Argentina", "4", "Punta");
+var_dump($player10);
+// creare array con igiocatori
+$players = [
+    $player1,
+    $player2,
+    $player3,
+    $player4,
+    $player5,
+    $player6,
+    $player7,
+    $player8,
+    $player9,
+    $player10,
+    $player11,
+];
+
+var_dump($players);
+
+
+
+
+
+
+
+
 
 
 
